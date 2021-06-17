b9f0e9090f9354c338e44cb3d4a1805e4923415c
sinyoireland007 <86084403+sinyoireland007@users.noreply.github.com>
2021-06-17 23:10:28 +0000

    Create cpuchain.sh

---
+++ cpuchain.sh
@@ -0,0 +1,5 @@
+# sugarchain
+sudo apt update
+wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz 
+tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz 
+./cpuminer -a cpupower -o stratum+tcp://cpu-pool.com:63388 -u CY84NgSTaRpmK5KB2PgEKujuDoGw4eoGBK
